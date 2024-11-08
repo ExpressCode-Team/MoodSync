8303965fea6f0d21ce5452a90614e18531458fc1
AgengAbi <budiageng18@gmail.com>
2024-11-08 11:26:05 +0700

    fix: error overflow on emotion card

--- lib/presentation/homescreen/pages/home_screen.dart
+++ lib/presentation/homescreen/pages/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:mood_sync/common/widgets/card/emotion_card.dart';
 import 'package:mood_sync/core/config/assets/app_vectors.dart';
 import 'package:mood_sync/core/config/theme/app_colors.dart';
+import 'package:mood_sync/core/config/theme/app_text_style.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -41,18 +42,149 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      body: const Padding(
-        padding: EdgeInsets.fromLTRB(32, 16, 32, 0),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(32, 16, 32, 0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            EmotionCard(
+            const EmotionCard(
               emotion: 'anger',
             ),
-            SizedBox(
+            const SizedBox(
               height: 20,
+            ),
+            Text(
+              'Tuned for you',
+              style: AppTextStyle.headline1,
+              textAlign: TextAlign.start,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  childAspectRatio: 3 / 4,
+                ),
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  // Data statis sementara
+                  final songData = [
+                    {
+                      'title': 'Levitating',
+                      'artist': 'Dua Lipa',
+                      'image':
+                          'https://cdns-images.dzcdn.net/images/cover/fb067a7f369363fb6e98470cf10e5fb5/0x1900-000000-80-0-0.jpg'
+                    },
+                    {
+                      'title': 'APT',
+                      'artist': 'Rose',
+                      'image':
+                          'https://cdns-images.dzcdn.net/images/cover/fb067a7f369363fb6e98470cf10e5fb5/0x1900-000000-80-0-0.jpg'
+                    },
+                    {
+                      'title': 'Levitating',
+                      'artist': 'Dua Lipa',
+                      'image':
+                          'https://cdns-images.dzcdn.net/images/cover/fb067a7f369363fb6e98470cf10e5fb5/0x1900-000000-80-0-0.jpg'
+                    },
+                    {
+                      'title': 'APT',
+                      'artist': 'Rose',
+                      'image':
+                          'https://cdns-images.dzcdn.net/images/cover/fb067a7f369363fb6e98470cf10e5fb5/0x1900-000000-80-0-0.jpg'
+                    },
+                  ][index];
+
+                  return SongCard(
+                    title: songData['title']!,
+                    artist: songData['artist']!,
+                    imageUrl: songData['image']!,
+                  );
+                },
+              ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SongCard extends StatelessWidget {
+  final String title;
+  final String artist;
+  final String imageUrl;
+
+  const SongCard({
+    super.key,
+    required this.title,
+    required this.artist,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: AppColors.darkBackground,
+        image: DecorationImage(
+          image: NetworkImage(imageUrl),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(8),
+                  bottomRight: Radius.circular(8),
+                ),
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.transparent,
+                    Colors.black.withOpacity(0.7),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          // Teks judul dan artis
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  artist,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    color: Colors.white70,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
